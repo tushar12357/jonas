7b7dc2148e57aff95e9cf55207109594f72647d3
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-07-21 19:41:56 +0530

    enquire now

--- src/components/EnquireNow.tsx
+++ src/components/EnquireNow.tsx
@@ -8,7 +8,7 @@ const EnquireNow: React.FC<EnquireNowProps> = ({ scrollToContact }) => {
   return (
     <button
       onClick={scrollToContact}
-      className="fixed bottom-4 right-4 bg-yellow-200 text-black px-6 py-3 rounded-full text-lg font-bold tracking-wider transition-colors duration-300 hover:bg-yellow-500 z-50"
+      className="fixed bottom-4 text-red-600 border-red-600 border-2 right-4 bg-yellow-200 text-black px-6 py-3 rounded-full text-lg font-bold tracking-wider transition-colors duration-300 hover:bg-yellow-500 z-50"
     >
       ENQUIRE NOW
     </button>
